e5bf3e92ca99e095565510ea8504b3c0debba99f
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-11-27 10:49:32 -0800

    [JENKINS-14843]
    We can't merge the proposed patch as is, and we should switch to POST if that's what has to happen.
    In the mean time, at least provide an escape hatch to allow people to bypass the team extension.
    
    Run Jenkins with -Dhudson.plugins.openid.impl.TeamsExtension.disable=true to disable the team extension.

--- src/main/java/hudson/plugins/openid/impl/TeamsExtension.java
+++ src/main/java/hudson/plugins/openid/impl/TeamsExtension.java
@@ -57,6 +57,8 @@ import java.util.logging.Logger;
 public class TeamsExtension extends OpenIdExtension {
     @Override
     public void extend(AuthRequest authRequest) throws MessageException {
+        if (DISABLE)    return;
+
         TeamExtensionRequest req = new TeamExtensionRequest();
         Collection<String> groups = Hudson.getInstance().getAuthorizationStrategy().getGroups();
         req.setQueryMembership(groups);
@@ -69,6 +71,8 @@ public class TeamsExtension extends OpenIdExtension {
 
     @Override
     public void process(AuthSuccess authSuccess, Identity id) throws MessageException {
+        if (DISABLE)    return;
+
         TeamExtensionResponse ter = getMessageAs(TeamExtensionResponse.class,  authSuccess, TeamExtensionFactory.URI);
         List<GrantedAuthority> r = id.getGrantedAuthorities();
         for (String s : ter.getTeamMembership())
@@ -85,4 +89,9 @@ public class TeamsExtension extends OpenIdExtension {
     }
 
     private static final Logger LOGGER = Logger.getLogger(TeamsExtension.class.getName());
+
+    /**
+     * Escape hatch for people affected by JENKINS-14843 until we switch to POST.
+     */
+    public static boolean DISABLE = Boolean.getBoolean(TeamsExtension.class.getName()+"disable");
 }
